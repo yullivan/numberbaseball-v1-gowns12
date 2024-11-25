e88b616f343c87581b20f04df02d3436614f880e
wjdgowns <wjdgowns4414@gmail.com>
2024-11-25 16:15:17 +0900

    숫자야구 구현

--- src/main/java/numberbaseball/Application.java
+++ src/main/java/numberbaseball/Application.java
@@ -1,5 +1,6 @@
 package numberbaseball;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
@@ -9,6 +10,18 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다!");
         System.out.println("1부터 9까지의 서로 다른 숫자 3개를 맞춰보세요.");
+
+        int[] answer = new int[3];
+        Random r = new Random();
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = r.nextInt(9) + 1;
+            for (int j = 0; j < i; j++) {
+                if (answer[i] == answer[j]) {
+                    i--;
+                }
+            }
+        }
+
 
         while (true) {
             // 사용자 입력 받기 (이 부분은 완성되어 있음)
@@ -20,17 +33,34 @@ public class Application {
             int user3 = scanner.nextInt();
 
             // TODO: strike 개수를 계산하세요
-
+            int strikes = 0;
+            for (int i : answer) {
+                if (user1 == i) {
+                    strikes++;
+                }
+            }
+            for (int i : answer) {
+                if (user2 == i) {
+                    strikes++;
+                }
+            }
+            for (int i : answer) {
+                if (user3 == i) {
+                    strikes++;
+                }
+            }
 
             // TODO: 결과를 출력하세요 (예: "1 스트라이크")
-
+            System.out.println(strikes + " 스트라이크");
 
             // TODO: 3 스트라이크인 경우 게임을 끝내세요
-
+            if (strikes==3){
+                break;
+            }
 
         }
 
-        // System.out.println("축하합니다! 정답을 맞추셨습니다.");
+         System.out.println("축하합니다! 정답을 맞추셨습니다.");
     }
 
 }
